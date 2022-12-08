89d3780443b905307b4023480bb556bc4d2baefd
Abidin Darıcı <114923176+abidindarici@users.noreply.github.com>
2022-12-08 14:48:44 +0300

    Add files via upload

---
+++ Celsius To Fahrenheit Program/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Celsius Degree:");
+Double celsius = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Temperature in celsius is: " + celsius);
+Double fahrenheit = ((celsius * 9) / 5) + 32;
+Console.WriteLine("Temperature in Fahrenheit is: " + fahrenheit);
+        
